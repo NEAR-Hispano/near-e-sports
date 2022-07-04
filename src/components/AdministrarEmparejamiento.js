95df27b6bbe9bdcd4bab6ef30a61c53bc9e6f6aa
unknown <carlos.morales@correo.unimet.edu.ve>
2022-07-04 13:18:53 -0400

    sistema de brackets funcional, falta usar datos de bd y poder editar el json

--- src/components/AdministrarEmparejamiento.js
+++ src/components/AdministrarEmparejamiento.js
@@ -3,36 +3,171 @@ import { useEffect, useState } from "react";
 import ProfileCardEquipo from "./cards/ProfileCardEquipo";
 import {
     Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
-    Button
+    Button,
+    Modal
 } from "reactstrap";
 
 // Core Components
 
-function AdministrarEmparejamiento() {
+function AdministrarEmparejamiento(props) {
 
-    const [comboActivo, setComboActivo] = useState(false)
-    const usarCombobox = () => {
-        setComboActivo(!comboActivo)
+    //---------------------------- Manejo de COMBOBOX -----------------------------
+    const [comboEmparejamientoActivo, setComboEmparejamientoActivo] = useState(false)
+    const usarComboboxEmparejamiento = () => {
+        setComboEmparejamientoActivo(!comboEmparejamientoActivo)
     }
+
+    const [comboRondaActivo, setComboRondaActivo] = useState(false)
+    const usarComboboxRonda = () => {
+        setComboRondaActivo(!comboRondaActivo)
+    }
+
+
+    const imprimirEmparejamientos = () => {
+        console.log(rondaSeleccionada)
+    }
+
+    //-------------------- Arreglos y Map COMBOBOX ------------------------
+    const [arrayRondas, setArrayRondas] = useState([])
+    const [arrayEmparejamientos, setArrayEmparejamientos] = useState([])
+    const [mapEmparejamientos, setMapEmparejamientos] = useState(new Map())
+
+    //-------------------- Manejo de Selecciones -------------------------
+    const [rondaSeleccionada, setRondaSeleccionada] = useState("")
+    const [equipoA, setEquipoA] = useState("Equipo A")
+    const [puntajeA, setPuntajeA] = useState(0)
+    const [resultadoA, setResultadoA] = useState("")
+    const [equipoB, setEquipoB] = useState("EquipoB")
+    const [puntajeB, setPuntajeB] = useState(0)
+    const [resultadoB, setResultadoB] = useState("")
+
+    const [show, setShow] = useState(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const cambiarRonda = (event) => {
+
+        let clave = String(event.currentTarget.textContent)
+
+        llenarEmparejamientosRonda(clave)
+
+    }
+
+    const llenarEmparejamientosRonda = (clave) => {
+
+        let arrayInformacionEmparejamientosRonda = []
+
+        setRondaSeleccionada(clave)
+
+        let arrayEmparejamientoRonda = mapEmparejamientos.get(clave)
+        arrayEmparejamientoRonda.map(informacionEmparejamiento => {
+
+            let i = 0
+            let equipoA, equipoB = ""
+            informacionEmparejamiento.teams.map(equipo => {
+                if (i == 0) {
+                    equipoA = equipo.name
+                } else {
+                    equipoB = equipo.name
+                }
+                i = i + 1
+            })
+
+            arrayInformacionEmparejamientosRonda.push({
+                'id': informacionEmparejamiento.id,
+                'fecha': informacionEmparejamiento.date,
+                'equipoA': equipoA,
+                'equipoB': equipoB,
+                'puntajeA': 0,
+                'puntajeB': 0,
+                'resultadoA': "",
+                'resultadoB': ""
+            })
+
+        })
+
+        setArrayEmparejamientos(arrayInformacionEmparejamientosRonda)
+        console.log(arrayInformacionEmparejamientosRonda)
+    }
+
+    useEffect(() => {
+
+        const cantidadRondas = props.rounds.length
+        let arrayInformacionRondas = [...props.rounds]
+        //setArrayEmparejamientos(array)
+        arrayInformacionRondas.map((informacionRonda) => {
+            mapEmparejamientos.set(informacionRonda.title, informacionRonda.seeds)
+        })
+
+        switch (cantidadRondas) {
+            case 1:
+
+                arrayRondas.push("Final")
+                setRondaSeleccionada("Final")
+                llenarEmparejamientosRonda("Final")
+
+            case 2:
+
+                arrayRondas.push("Semi-Final")
+                arrayRondas.push("Final")
+                setRondaSeleccionada("Semi-Final")
+                llenarEmparejamientosRonda("Semi-Final")
+
+            case 3:
+
+                arrayRondas.push("4tos")
+                arrayRondas.push("Semi-Final")
+                arrayRondas.push("Final")
+                setRondaSeleccionada("4tos")
+                llenarEmparejamientosRonda("4tos")
+
+            case 4:
+
+                arrayRondas.push("8vos")
+                arrayRondas.push("4tos")
+                arrayRondas.push("Semi-Final")
+                arrayRondas.push("Final")
+                setRondaSeleccionada("8vos")
+                llenarEmparejamientosRonda("8vos")
+
+        }
+
+    }, []);
 
     return (
         <div className="container-fluid">
             <br />
             <div className="row">
-                <div className="col-5"></div>
-                <Dropdown className="col-2" 
-                isOpen={comboActivo} 
-                toggle={usarCombobox}
-                color="light">
+                <Dropdown className=""
+                    isOpen={comboRondaActivo}
+                    toggle={usarComboboxRonda}
+                    color="light">
+                    <DropdownToggle caret>
+                        Ronda
+                    </DropdownToggle>
+                    <DropdownMenu left>
+                        {arrayRondas.map((rondaNombre) => (
+                            <DropdownItem
+                                key={rondaNombre}
+                                onClick={cambiarRonda}>{rondaNombre}</DropdownItem>
+                        ))}
+                    </DropdownMenu>
+                </Dropdown>
+                <br />
+                <br />
+                <Dropdown className=""
+                    isOpen={comboEmparejamientoActivo}
+                    toggle={usarComboboxEmparejamiento}
+                    color="light">
                     <DropdownToggle caret>
                         Emparejamientos
                     </DropdownToggle>
-                    <DropdownMenu right>
-                        <DropdownItem>A Vs B</DropdownItem>
-                        <DropdownItem>C Vs D</DropdownItem>
+                    <DropdownMenu left>
+                        {arrayEmparejamientos.map((data) => (
+                            <DropdownItem>{data.equipoA + " - " + data.equipoB}</DropdownItem>
+                        ))}
                     </DropdownMenu>
                 </Dropdown>
-                <div className="col-5"></div>
             </div>
             <br />
             <div className="row">
@@ -40,38 +175,32 @@ function AdministrarEmparejamiento() {
                 </div>
                 <div className="col-3">
                     <ProfileCardEquipo
-                        nombre="Team A"
+                        nombre={equipoA}
                         img="https://pm1.narvii.com/6637/8610c455a09a98f950df116322de26207e98a8b6_hq.jpg"
-                        resultado="Ganador" />
+                        resultado={resultadoA} />
                 </div>
                 <div className="col-2 row-container-3">
 
                     <div className="row row-item-2">
-                        <div className="col-3">
+                        <div className="text-center">
+                            <h1>{puntajeA + " - " + puntajeB}</h1>
                         </div>
-                        <div className="col-2">
-                            <h1 className="text-blanco text-center">3</h1>
-                        </div>
-                        <div className="col-2">
-                            <h1 className="text-blanco text-center">-</h1>
-                        </div>
-                        <div className="col-2">
-                            <h1 className="text-blanco text-center">2</h1>
-                        </div>
-                        <div className="col-3"></div>
                     </div>
-                    <div className="row-item-3 text-center">
-                        <Button color="warning" type="button">
-                            Administrar
+                    <div className="row-item-3 text-center text-blanco">
+                        <Button
+                            variant="danger"
+                            className="bg-danger text-blanco"
+                            onClick={imprimirEmparejamientos}>
+                            Editar
                         </Button>
                     </div>
 
                 </div>
                 <div className="col-3">
                     <ProfileCardEquipo
-                        nombre="Team B"
+                        nombre={equipoB}
                         img="https://esports.eldesmarque.com/wp-content/uploads/2017/05/SKT-Untara.jpg"
-                        resultado="Perdedor" />
+                        resultado={resultadoB} />
                 </div>
                 <div className="col-2">
                 </div>
--- src/components/Brackets.js
+++ src/components/Brackets.js
@@ -1,192 +1,237 @@
 import { Bracket, RoundProps } from 'react-brackets';
 import React from "react";
+import { useEffect, useState } from "react";
+import {
+    Button
+} from "reactstrap";
+import AdministrarEmparejamiento from './AdministrarEmparejamiento';
 
-const rounds = [
-    {
-        title: 'Round one',
-        seeds: [
-            {
-                id: 1,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 2,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 3,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 4,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 5,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 6,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 7,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 8,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 9,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 10,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 11,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 12,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 13,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 14,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 15,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-            {
-                id: 16,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team C' }, { name: 'Team D' }],
-            },
-        ],
-    },
-    {
-        title: 'Round two',
-        seeds: [
-            {
-                id: 17,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 18,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 19,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 20,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 21,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 22,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 23,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 24,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-        ]
-    },
-    {
-        title: 'Round Cuartos',
-        seeds: [
-            {
-                id: 25,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 26,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 27,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 28,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-           ]
-    },
-    {
-        title: 'Round  Semi',
-        seeds: [
-            {
-                id: 29,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-            {
-                id: 30,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-           ]
-    },
-    {
-        title: 'Round  final',
-        seeds: [
-            {
-                id: 31,
-                date: new Date().toDateString(),
-                teams: [{ name: 'Team A' }, { name: 'Team B' }],
-            },
-           ]
-    },
+function RenderBracket(props){
+
+    //Arreglos con la informacion de cada ronda del torneo
+    //Estado
+    const [edicion, setEdicion] = useState(false)
+    const changeEdicion = () => {
+        setEdicion(!edicion)
+    }
+
+    const roundDieciseisAvos = [
+        {
+            title: 'Ronda Inicial',
+            seeds: [
+                {
+                    id: 1,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 2,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 3,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 4,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 5,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 6,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 7,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 8,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 9,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 10,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 11,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 12,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 13,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 14,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 15,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+                {
+                    id: 16,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team C' }, { name: 'Team D' }],
+                },
+            ],
+        }
+    ]
     
-];
+    const rounds = [
+        {
+            title: '8vos',
+            seeds: [
+                {
+                    id: 17,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 18,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 19,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 20,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 21,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 22,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 23,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 24,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+            ]
+        },
+        {
+            title: '4tos',
+            seeds: [
+                {
+                    id: 25,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 26,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 27,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+                {
+                    id: 28,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Team A' }, { name: 'Team B' }],
+                },
+            ]
+        },
+        {
+            title: 'Semi-Final',
+            seeds: [
+                {
+                    id: 29,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Kevin' }, { name: 'Carlos' }],
+                },
+                {
+                    id: 30,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Luis' }, { name: 'Jose' }],
+                },
+            ]
+        },
+        {
+            title: 'Final',
+            seeds: [
+                {
+                    id: 31,
+                    date: new Date().toDateString(),
+                    teams: [{ name: 'Kevin' }, { name: 'Luis' }],
+                },
+            ]
+        },
+    
+    ];
 
-const RenderBracket = () => {
-    return <Bracket rounds={rounds} />;
+    return (
+        <div>
+            <div className="">
+                {!edicion ? 
+                        <div>
+                            <Button 
+                            color="primary" 
+                            type="button"
+                            onClick={changeEdicion}>
+                                Administrar Resultados
+                            </Button>
+                            <br />
+                            <br />
+                        </div>
+                    :
+                        <Button 
+                        color="primary" 
+                        type="button"
+                        onClick={changeEdicion}>
+                            Volver
+                        </Button>
+                }
+            </div>
+            {!edicion ?
+                <Bracket rounds={rounds} />
+            :
+                <AdministrarEmparejamiento rounds={rounds}/>
+            }
+        </div>
+    )
 };
 
 export default RenderBracket;
