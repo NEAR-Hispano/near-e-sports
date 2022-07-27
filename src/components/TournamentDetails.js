75103764c69d54d6361c4f0e9940fa8536b6125e
unknown <carlos.morales@correo.unimet.edu.ve>
2022-07-26 22:08:43 -0400

    arreglar generacion de brackets 3 equipos

--- src/components/TournamentDetails.js
+++ src/components/TournamentDetails.js
@@ -37,8 +37,7 @@ function TorneosDetalles() {
             idteam: 2,
             name: "El Two",
             owner: "two,test"
-        },
-        {
+        },{
             idteam: 3,
             name: "El Tri",
             owner: "Tri,test"
@@ -51,28 +50,61 @@ function TorneosDetalles() {
             idteam: 5,
             name: "El Five",
             owner: "five,test"
-        },
-        {
+        },{
             idteam: 6,
             name: "El Six",
             owner: "Six,test"
-        },
-        {
+        },{
             idteam: 7,
             name: "El Seven",
             owner: "Seven,test"
-        },
-        {
+        },{
             idteam: 8,
-            name: "El Seven",
+            name: "El Ocho",
             owner: "Seven,test"
+        },{
+            idteam: 9,
+            name: "El Nueve",
+            owner: "Seven,test"
+        },{
+            idteam: 10,
+            name: "El 10",
+            owner: "one,test"
         },
         {
-            idteam: 9,
-            name: "El Seven",
-            owner: "Seven,test"
-        }
-
+            idteam: 11,
+            name: "El 11",
+            owner: "two,test"
+        },
+        {
+            idteam: 12,
+            name: "El 12",
+            owner: "two,test"
+        },
+        {
+            idteam: 13,
+            name: "El 13",
+            owner: "two,test"
+        },
+        {
+            idteam: 14,
+            name: "El 14",
+            owner: "two,test"
+        },{
+            idteam: 15,
+            name: "El 15",
+            owner: "two,test"
+        },
+        {
+            idteam: 16,
+            name: "El 16",
+            owner: "two,test"
+        },
+        {
+            idteam: 17,
+            name: "El 17",
+            owner: "two,test"
+        },
     ]
 
     const [hTabsIcons, setHTabsIcons] = React.useState("hTabsIcons-1");
